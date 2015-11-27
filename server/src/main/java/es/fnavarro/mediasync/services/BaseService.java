d4e279618d3af7d2d6ba986deb94e5c249905ec0
packarbell@gmail.com <packarbell@gmail.com>
2015-11-27 12:21:35 +0100

    Create embebed database

--- server/src/main/java/es/fnavarro/mediasync/services/BaseService.java
+++ server/src/main/java/es/fnavarro/mediasync/services/BaseService.java
@@ -1,5 +1,10 @@
 package es.fnavarro.mediasync.services;
 
+import org.apache.log4j.Logger;
+
 public class BaseService {
+	
+	protected static final Logger logger = Logger.getLogger(BaseService.class);
+
 
 }
--- server/src/main/java/es/fnavarro/mediasync/services/CheckDataBaseService.java
+++ server/src/main/java/es/fnavarro/mediasync/services/CheckDataBaseService.java
@@ -1,16 +1,106 @@
 package es.fnavarro.mediasync.services;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("checkDataBaseService")
-public class CheckDataBaseService {
-	
+public class CheckDataBaseService extends BaseService{
+
+	private static final String TABLES[]  = { "config", "files" };
+
+	private static final String SQL_PATH = "sql/";
+
+	private DatabaseMetaData metaData; 
+	private Connection connection;
+
 	@Autowired
 	public void checkIfExists(DataSource datasource){
-		System.out.println(datasource);
+		
+		try{
+			connection = datasource.getConnection();
+			metaData = connection.getMetaData();			
+			for(String table : TABLES ){
+				checkOrCreateTable(table);
+			}
+
+		}catch(Exception e){
+			logger.error("Exception checking database", e);
+			
+		}finally {
+			closeConnection();
+		}
 	}
+
+	private void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {				
+			e.printStackTrace();
+		}
+	}
+
+	private void checkOrCreateTable(String tablePattern) throws Exception {
+
+		ResultSet tables = metaData.getTables(null, null, tablePattern , null);
+
+		if (tables.next()) {
+			logger.debug("Table "+tablePattern+" exists "); 
+
+		} else{
+			createTable(tablePattern);
+		}
+	}
+
+	private void createTable(String table) throws Exception{
+		logger.debug("Creating table " + table);
+		PreparedStatement statement = null;
+		try{
+			statement = connection.prepareStatement(getSqlResource(table));			
+			statement.executeUpdate();
+			
+		}catch(Exception e){
+			logger.error("Exception creating table "+table, e);
+			throw e;
+		} finally {
+			closeStatement(statement);
+		}
+		
+	}
+
+	private void closeStatement(PreparedStatement statement) {
+		if(statement!=null){
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private String getSqlResource(String table) throws Exception{
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream(SQL_PATH + table + ".sql");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		StringBuilder builder = new StringBuilder();
+		
+		String line=null;
+		while((line=reader.readLine())!=null){
+		    builder.append(line).append("\n");
+		}
+		
+		return builder.toString();		
+	}
+	
+	
 
 }
