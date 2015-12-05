7441069fa9aec7f0dfc1a1d878725dfc3e2fe75e
packarbell@gmail.com <packarbell@gmail.com>
2015-12-05 03:20:49 +0100

    Filter to show filesize

---
+++ angularApp/src/client/app/files/filesize.filter.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.files')
+        .filter('fileSizeFilter', [
+            'numberFilter',
+
+            function fileSizeFilter(numberFilter) {
+
+                var units = ['B', 'KB', 'MB', 'GB'];
+                return function(input) {
+
+                    if (input) {
+                        return divide(input, 0);
+                    }
+
+                };
+
+                function divide(number, base) {
+                    if (number < 1024) {
+                        return parseInt(number) + ' ' + units[base];
+                    }
+                    return divide(parseInt(number) / 1024, base + 1);
+                }
+
+
+            }
+        ]);
+}());
